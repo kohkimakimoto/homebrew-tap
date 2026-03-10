1136f690310e5f3e538f0bbda116b5db8989507e
Kohki Makimoto <kohki.makimoto@gmail.com>
2026-03-10 16:20:13 +0900

    update

--- Formula/claude-sandbox.rb
+++ Formula/claude-sandbox.rb
@@ -1,16 +1,16 @@
 class ClaudeSandbox < Formula
   desc "A wrapper tool to safely run Claude Code in a sandboxed environment on macOS"
   homepage "https://github.com/kohkimakimoto/claude-sandbox"
-  version "2.1.0"
+  version "2.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kohkimakimoto/claude-sandbox/releases/download/v#{version}/claude-sandbox_#{version}_darwin_arm64.tar.gz"
-      sha256 "646f092017ba2ffa0cefd0aacf894c2dbe56127234bd7daa1b2cd57d998c42dc"
+      sha256 "4d60ecca80a2854804698bbac564ff3fcd452560ad7681d24ccca80ea3d77397"
     else
       url "https://github.com/kohkimakimoto/claude-sandbox/releases/download/v#{version}/claude-sandbox_#{version}_darwin_amd64.tar.gz"
-      sha256 "0121c73cd1ba107b045099c4b6580594dd24d6933698f65c960b2e7f69a96064"
+      sha256 "bf8ea57aa7be31fd7f6fc5523b596c637ba56402e8775a7aeda7d24f674719d7"
     end
   end
 
