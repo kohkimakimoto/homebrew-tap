8788868b1571108fa401b705a30f960e6ad00045
Kohki Makimoto <kohki.makimoto@gmail.com>
2026-03-17 15:11:33 +0900

    Bump version to 2.3.0 and update checksums

--- Formula/claude-sandbox.rb
+++ Formula/claude-sandbox.rb
@@ -1,16 +1,16 @@
 class ClaudeSandbox < Formula
   desc "A wrapper tool to safely run Claude Code in a sandboxed environment on macOS"
   homepage "https://github.com/kohkimakimoto/claude-sandbox"
-  version "2.2.0"
+  version "2.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kohkimakimoto/claude-sandbox/releases/download/v#{version}/claude-sandbox_#{version}_darwin_arm64.tar.gz"
-      sha256 "4d60ecca80a2854804698bbac564ff3fcd452560ad7681d24ccca80ea3d77397"
+      sha256 "58001bb2549caf190b682b3c4e8291798b3d4f37c5aeb489e993fa1f55fbbd61"
     else
       url "https://github.com/kohkimakimoto/claude-sandbox/releases/download/v#{version}/claude-sandbox_#{version}_darwin_amd64.tar.gz"
-      sha256 "bf8ea57aa7be31fd7f6fc5523b596c637ba56402e8775a7aeda7d24f674719d7"
+      sha256 "e9dc7da7e4e34021e4a7851dd532c3b1ae6787a27f83208190113b971c0d7504"
     end
   end
 
